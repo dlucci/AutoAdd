02b3aed9d766a756c9b136909098f898caf5bade
Derril <dlucci@zeus.(none)>
2013-08-21 05:41:24 -0400

    Initial commit

---
+++ src/com/example/autosend/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.autosend;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.util.Log;
+
+import com.example.autosend.R;
+
+public class MainActivity extends Activity implements OnClickListener{
+
+	private static final String TAG = "MainActivity";
+
+    private EditText editText;
+	private Button done;
+
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        editText = (EditText) findViewById(R.id.messageText);
+        done = (Button) findViewById(R.id.done);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    @Override
+    public void onClick(View target)
+    {
+    	int id = target.getId();
+    	if(id == done.getId())
+    	{
+    		String text = editText.toString();
+    		Log.i(TAG, String.valueOf(text));
+    	}
+    }
+    
+}
