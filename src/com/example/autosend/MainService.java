02fbeb6736deb60d3193fcad23cc1a09abffa59f
Derril <dlucci@zeus.(none)>
2013-09-04 05:50:01 -0400

    Working on getting the cursor to work.  I think my query might be wrong

--- src/com/example/autosend/MainService.java
+++ src/com/example/autosend/MainService.java
@@ -1,11 +1,14 @@
 package com.example.autosend;
 
 import android.app.Service;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.ContentObserver;
+import android.database.Cursor;
 import android.os.IBinder;
 import android.provider.Contacts.People;
 import android.provider.ContactsContract;
+import android.provider.ContactsContract.RawContacts;
 import android.util.Log;
 
 
@@ -18,6 +21,9 @@ public class MainService extends Service{
 	
 	public static final String TAG = "MainService";
 	private MyContentObserver observer;
+	private ContentResolver cr;
+	
+	
 	@Override
 	public void onCreate()
 	{
@@ -25,6 +31,7 @@ public class MainService extends Service{
 		
 		Log.d(TAG, "onCreate()");
 		observer = new MyContentObserver();
+		cr = getContentResolver();
 		this.getApplicationContext().getContentResolver().registerContentObserver(ContactsContract.Contacts.CONTENT_URI, false, observer);
 		
 	}
@@ -38,7 +45,18 @@ public class MainService extends Service{
         @Override
         public void onChange(boolean selfChange) {
             super.onChange(selfChange);
-            Log.i(TAG, "contact change!");
+            
+            Cursor c = cr.query(RawContacts.CONTENT_URI,
+                    new String[]{RawContacts.DATA_SET},
+                    RawContacts.DIRTY + "=?",
+                    new String[]{String.valueOf(1)}, null);
+            Log.i(TAG, String.valueOf(c.getCount()));
+            while(c.moveToFirst())
+            {
+            	String data = c.getString(c.getColumnIndex(RawContacts.DATA_SET));
+            	Log.i(TAG, String.valueOf(data));
+            }
+
         }
 	}
 	
